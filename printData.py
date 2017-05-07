87fb8d28e0ea50f346c396e22bd395c3f7faaa20
abigailyuan <abigail_yuan@hotmail.com>
2017-05-07 14:37:11 +1000

    add parseText feature

--- printData.py
+++ printData.py
@@ -5,11 +5,65 @@ from pprint import pprint
 
 import collections
 
-with open('dev.json') as json_data:
+
+def countFile(filename):
+    '''count number of instances for each language in the file'''
+    json_data = open(filename)
     data = json.load(json_data)
     #print(data)
     langDict = dd(int)
     for item in data:
         lang = item.get("lang")
         langDict[lang] += 1
-    print(langDict) 
+    #print("-----------dev data-------------")
+    for key in langDict.keys():
+        print(key+": "+str(langDict[key]))
+    json_data.close()
+    return langDict
+
+def getTextList(filename):
+    json_data = open(filename)
+    data = json.load(json_data)
+    textList = []
+    for item in data:
+        text = item.get('text').split()
+        textList.append(text)
+    return textList
+def parseText(filename):
+    json_data = open(filename)
+    data = json.load(json_data)
+    dataList = []
+    for item in data:
+        text = item.get('text').split()
+        item['text']= text
+        itemDict = {'lang': 'unknown', 'displayname': 'unknown', 'location': 'unknown', 'text':'unknown'}
+        for key in item.keys():
+            if(key != 'uid'):
+                itemDict[key] = item[key]
+        dataList.append(itemDict)
+    return dataList
+
+def readToList(filename):
+    '''read to a list of instances while each instance is a dictionary'''
+    fileList = []
+    json_data = open(filename)
+    data = json.load(json_data)
+    for instance in data:
+        dataDict = {'lang': null, 'displayname': null, 'location': null, 'text': null}
+        
+
+
+
+
+
+##print("----------dev file--------------")
+##countFile('dev.json')
+##print("------------train file---------")
+##countFile('train.json')
+##text = getTextList('dev.json')
+##for i in text:
+##    print(i)
+print("----------parsed data-------------")
+dataList = parseText('dev.json')
+for i in dataList:
+    print(i)
