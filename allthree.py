0165c8f66654173a6b505e332e1101e3e5701089
abigailyuan <abigail_yuan@hotmail.com>
2017-05-12 00:46:10 +1000

    final version:createfile.py

---
+++ allthree.py
@@ -0,0 +1,73 @@
+import json
+import numpy as np
+import re
+from collections import defaultdict as dd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.decomposition import PCA
+from sklearn.naive_bayes import GaussianNB
+from sklearn import svm
+
+
+#####################preprocessing###################
+fp = open('devfile.json')
+data = []
+target = []
+
+for line in fp:
+    instance = json.loads(line)
+    target.append(instance['lang'])
+    data.append(instance['text'])
+
+categories = {'ar': 1,
+              'bg': 2,
+              'de': 3,
+              'en': 4,
+              'es': 5,
+              'fa': 6,
+              'fr': 7,
+              'he': 8,
+              'hi': 9,
+              'it': 10,
+              'ja': 11,
+              'ko': 12,
+              'mr': 13,
+              'ne': 14,
+              'nl': 15,
+              'ru': 16,
+              'th': 17,
+              'uk': 18,
+              'ur': 19,
+              'zh': 20,
+              'unk': 21}
+target = np.array(target)
+for i in range(len(target)):
+    target[i] = categories[target[i]]
+print(target)
+data = np.array(data)
+hv = HashingVectorizer(n_features=100, token_pattern=r'\b\w+\b',ngram_range=(2,6), analyzer='char_wb')
+X = hv.transform(data).toarray()
+transformer = TfidfTransformer(smooth_idf=False)
+X2 = transformer.fit_transform(X).toarray()
+########################classifier starts now###################
+##'''GaussianNB'''
+##clf = GaussianNB()
+##clf.fit(X2, target)
+##score = clf.score(X2, target)
+##print('NB score = '+str(score))
+
+'''Decision tree'''
+##one_r = DecisionTreeClassifier()
+##one_r.fit(X2[:8000], target[:8000])
+##score = one_r.score(X2[8000:], target[8000:])
+##print('Decision tree score = '+str(score))
+
+'''SVM'''
+clf = svm.SVC()
+clf.fit(X2, target)
+score = clf.score(X2, target)
+print('SVM score = '+str(score))
