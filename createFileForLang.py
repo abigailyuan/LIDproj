80890ac95d5eb1e3fad0082452dd023ad1e5d5e9
abigailyuan <abigail_yuan@hotmail.com>
2017-05-11 14:09:56 +1000

    create file for languages

---
+++ createFileForLang.py
@@ -0,0 +1,20 @@
+import json
+from collections import defaultdict as dd
+
+def createFile(filename):
+    f = open(filename)
+    data_dict = dd(list)
+    for line in f:
+        j_content = json.loads(line)
+        data_dict[j_content['lang']].append(j_content['text'])
+    f.close()
+    for lang in data_dict.keys():
+        filename = lang+'.json'
+        f = open(filename, 'a')
+        for text in data_dict[lang]:
+            f.write(text)
+        print('finished one file')
+        f.close()
+
+createFile('trainfile.json')
+            
